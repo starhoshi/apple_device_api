78858d15462b65612295378deff78817b9f2478f
starhoshi <kensuke1751@gmail.com>
2017-04-23 11:21:04 +0900

    output json.

--- yml_to_json.rb
+++ yml_to_json.rb
@@ -5,7 +5,9 @@ require 'rake'
 
 Dir.glob('data/**/{[!template]}*.yml').each do | yml_filename |
   p yml_filename
-  output_filename = yml_filename.sub(/(yml|yaml)$/, 'json')
+  output_filename = yml_filename
+                        .sub(/(yml|yaml)$/, 'json')
+                        .sub(/(data)$/, 'api/v1')
 
   yml = File.open(yml_filename, 'r').read
   json = JSON.dump(YAML::load(yml))
