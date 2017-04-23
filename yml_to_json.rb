8f7049b6177907681f41093d2a892fb1f7f577bd
starhoshi <kensuke1751@gmail.com>
2017-04-23 11:42:43 +0900

    code format.

--- yml_to_json.rb
+++ yml_to_json.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'json'
 
-Dir.glob('data/**/{[!template]}*.yml').each do | yml_filepath |
+Dir.glob('data/**/{[!template]}*.yml').each do |yml_filepath|
   p "input yml flepath:   #{yml_filepath}"
 
   output_filename = yml_filepath
@@ -9,7 +9,7 @@ Dir.glob('data/**/{[!template]}*.yml').each do | yml_filepath |
                         .sub(/data/, 'api/v1/devices')
   p "output json flepath: #{output_filename}"
 
-   output_dir = output_filename.sub(File.basename(output_filename), '')
+  output_dir = output_filename.sub(File.basename(output_filename), '')
 
   FileUtils.mkdir_p(output_dir) unless FileTest.exist?(output_dir)
 
